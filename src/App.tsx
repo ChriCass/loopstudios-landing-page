b9b54c7d71389fd877ab6184dd097661586e59db
ChriCass <chris_ccc68@outlook.com>
2026-07-12 20:20:40 -0500

    fix: navbar hover - navigation links

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <>
       <div className="relative">
-        <div className="absolute top-0 left-0 right-0 z-10">
+        <div className="absolute top-0 left-0 right-0 z-20">
           <Navbar />
         </div>
         <Hero />
